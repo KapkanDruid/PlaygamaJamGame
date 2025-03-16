9995d71048e6097817272bb6d11ab252ec4fa899
KapkanDruid <robloxxmapcreator1970@gmail.com>
2025-03-16 04:54:47 -0700

    feat: добавлена система ввода

--- Assets/_Project/Scripts/Architecture/Installers/MainServicesSceneInstaller.cs
+++ Assets/_Project/Scripts/Architecture/Installers/MainServicesSceneInstaller.cs
@@ -1,3 +1,4 @@
+using Project.Content.BuildSystem;
 using UnityEngine;
 using Zenject;
 
@@ -6,14 +7,11 @@ namespace Project.Architecture
     public class MainServicesSceneInstaller : MonoInstaller
     {
         [SerializeField] private MainSceneBootstrap _sceneBootstrap;
-        [SerializeField] private Canvas _levelCanvas;
 
         public override void InstallBindings()
         {
             Container.Bind<MainSceneBootstrap>().FromInstance(_sceneBootstrap).AsSingle();
-
-            Container.Bind<Canvas>().FromInstance(_levelCanvas).AsSingle().NonLazy();
-
+            Container.Bind<InputSystemActions>().AsSingle().NonLazy();
         }
     }
 }
