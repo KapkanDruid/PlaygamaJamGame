db9f33e4b070c145bbb93be420e533f162947de7
KapkanDruid <robloxxmapcreator1970@gmail.com>
2025-03-18 05:46:44 -0700

    feat: добавлен EntityProvider

---
+++ Assets/_Project/Scripts/Content/EntityProvider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Project.Content
+{
+    public class EntityProvider : MonoBehaviour, IEntity
+    {
+        [SerializeField] private GameObject _entityReference;
+
+        private IEntity _entity;
+
+        private void Start()
+        {
+            _entity = _entityReference.GetComponent<IEntity>();
+        }
+
+        public T ProvideComponent<T>() where T : class
+        {
+            return _entity.ProvideComponent<T>();
+        }
+    }
+}
