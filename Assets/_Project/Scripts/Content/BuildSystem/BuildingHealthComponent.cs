dd16fba4cea589690c7d45dbfb219614e3fec740
KapkanDruid <robloxxmapcreator1970@gmail.com>
2025-03-18 03:02:02 -0700

    feat: добавлен BuildingHealthComponent

---
+++ Assets/_Project/Scripts/Content/BuildSystem/BuildingHealthComponent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace Project.Content.BuildSystem
+{
+    public class BuildingHealthComponent : IDamageable
+    {
+        private IHealthData _data;
+        private float _health;
+        private bool _isDead = false;
+
+        public event Action OnDead;
+        public float Health => _health;
+
+        public BuildingHealthComponent(IHealthData data)
+        {
+            _data = data;
+        }
+
+        public void Initialize()
+        {
+            _health = _data.Health;
+        }
+
+        public void TakeDamage(float damage, Action callback)
+        {
+            if (_isDead)
+                return;
+
+            callback?.Invoke();
+
+            Debug.Log($"Building take {damage} damage, current health: {_health}");
+
+            _health -= damage;
+
+            if (_health <= 0)
+            {
+                _isDead = true;
+                OnDead?.Invoke();
+                return;
+            }
+        }
+    }
+}
