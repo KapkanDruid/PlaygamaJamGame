5b15d61da16d738afe6ab77c150bc4f3e88022ff
KapkanDruid <robloxxmapcreator1970@gmail.com>
2025-03-21 01:10:01 -0700

    feat: добавлен TargetSensor

---
+++ Assets/_Project/Scripts/Content/BuildSystem/SensorLogic/ISensorData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Project.Content.BuildSystem
+{
+    public interface ISensorData
+    {
+        public Transform SensorOrigin { get; }
+        public float SensorRadius { get; }
+        public IEntity ThisEntity { get; }
+        public EntityFlags TargetFlag { get; }
+    }
+}
