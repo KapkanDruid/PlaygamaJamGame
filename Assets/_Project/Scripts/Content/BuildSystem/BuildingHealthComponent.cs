69574cb7101111d309be4bd1f3592181de0a3543
KapkanDruid <robloxxmapcreator1970@gmail.com>
2025-03-22 12:36:54 -0700

    feat: BuildingHealthComponent реагирует на изменения свойства Health

--- Assets/_Project/Scripts/Content/BuildSystem/BuildingHealthComponent.cs
+++ Assets/_Project/Scripts/Content/BuildSystem/BuildingHealthComponent.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 
 namespace Project.Content.BuildSystem
 {
-    public class BuildingHealthComponent : IDamageable
+    public class BuildingHealthComponent : IDamageable, IDisposable
     {
         private IHealthData _data;
         private IHealthView _view;
 
         private float _currentHealth;
+        private float _maxHealth;
         private bool _isDead = false;
 
         public event Action OnDead;
@@ -22,8 +23,25 @@ namespace Project.Content.BuildSystem
 
         public void Initialize()
         {
-            _currentHealth = _data.Health;
-            _view.SetHealth(_currentHealth, _data.Health);
+            _maxHealth = _data.Health.Value;
+            _currentHealth = _maxHealth;
+            _view.SetHealth(_currentHealth, _maxHealth);
+
+            _data.Health.OnValueChanged += OnMaxHealthChanged;
+        }
+
+        private void OnMaxHealthChanged(float changedMaxHealth)
+        {
+            Debug.Log("Changed health: " + changedMaxHealth);
+            float percent = (_currentHealth / _maxHealth) * 100;
+
+            _maxHealth = changedMaxHealth;
+            _currentHealth = (percent / 100f) * _maxHealth;
+
+            _view.SetHealth(_currentHealth, _maxHealth);
+
+            Debug.Log("Max Health " + _maxHealth);
+            Debug.Log("Current Health " + _currentHealth);
         }
 
         public void TakeDamage(float damage, Action callback)
@@ -42,7 +60,7 @@ namespace Project.Content.BuildSystem
         {
             _currentHealth += healthModifier;
 
-            _view.SetHealth(_currentHealth, _data.Health);
+            _view.SetHealth(_currentHealth, _maxHealth);
 
             if (_currentHealth <= 0)
             {
@@ -50,6 +68,11 @@ namespace Project.Content.BuildSystem
                 OnDead?.Invoke();
                 return;
             }
+        }
+
+        public void Dispose()
+        {
+            _data.Health.OnValueChanged -= OnMaxHealthChanged;
         }
     }
 }
