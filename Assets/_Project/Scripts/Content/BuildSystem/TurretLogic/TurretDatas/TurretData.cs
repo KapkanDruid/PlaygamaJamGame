59efb397727a50eb70ffe0a5a5630e2eb8f5e83a
KapkanDruid <robloxxmapcreator1970@gmail.com>
2025-03-22 00:30:37 -0700

    feat: добавлена логика динамичных данных

---
+++ Assets/_Project/Scripts/Content/BuildSystem/TurretLogic/TurretDatas/TurretData.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+namespace Project.Content.BuildSystem
+{
+    [Serializable]
+    public class TurretData : IPlaceComponentData, IHealthData, ITurretShootData, IProjectilePoolData, IDisposable
+    {
+        //[SerializeField] private float _maxHealth;
+        //[SerializeField] private float _fireRate;
+        //[SerializeField] private float _rotateSpeed;
+        //[SerializeField] private float _projectileLifeTime;
+        //[SerializeField] private float _projectileSpeed;
+        //[SerializeField] private float _projectileDamage;
+        //[SerializeField] private float _sensorRadius;
+        //[SerializeField] private float _rotationThreshold;
+        [SerializeField] private DirectProjectile _projectilePrefab;
+
+        [SerializeField] private Transform _gridPivotTransform;
+        [SerializeField] private SpriteRenderer[] _spriteRenderers;
+        [SerializeField] private Transform[] _scalableObjects;
+        [SerializeField] private GridPatternData _gridPattern;
+        [SerializeField] private Flags _flags;
+        [SerializeField] private GameObject[] _physicObjects;
+        [SerializeField] private Transform _turretRotationObject;
+        [SerializeField] private Transform _turretShootPoint;
+
+        [SerializeField] private EntityFlags _enemyFlag;
+        [SerializeField] private TurretType _turretType;
+
+        private DirectProjectileData _projectileData = new DirectProjectileData();
+        private SensorData _sensorData = new SensorData();
+        private TurretDynamicData _dynamicData;
+        private TurretConfig _configData;
+
+        private IEntity _thisEntity;
+
+        public float Health => _dynamicData.MaxHealth;
+        public float FireRate => _dynamicData.FireRate;
+        public int ProjectilePoolCount => 10;
+
+        public Transform PivotTransform => _gridPivotTransform;
+        public SpriteRenderer[] SpriteRenderers => _spriteRenderers;
+        public Transform[] ScalableObjects => _scalableObjects;
+        public GridPatternData GridPattern => _gridPattern;
+        public Flags Flags => _flags;
+        public GameObject[] PhysicObjects => _physicObjects;
+        public Transform RotationObject => _turretRotationObject;
+        public Transform ShootPoint => _turretShootPoint;
+        public DirectProjectile ProjectilePrefab => _projectilePrefab;
+
+        public IDirectProjectileData ProjectileData => _projectileData;
+        public ISensorData SensorData => _sensorData;
+        public IEntity ThisEntity => _thisEntity;
+        public float RotateSpeed => _configData.RotateSpeed;
+        public float RotationThreshold => _configData.RotationThreshold;
+
+        [Inject]
+        private void Construct(TurretDynamicData dynamicData, IEntity entity)
+        {
+            _dynamicData = dynamicData;
+            _thisEntity = entity;
+            _configData = _dynamicData.Config;
+
+            if (dynamicData.Type != _turretType)
+                Debug.LogError("Wrong type of data received by " + ToString());
+        }
+
+        public void Initialize()
+        {
+            _projectileData.EnemyFlag = _enemyFlag;
+            _projectileData.LifeTime = _configData.ProjectileLifeTime;
+            _projectileData.Speed = _configData.ProjectileSpeed;
+
+            _sensorData.SensorOrigin = _turretRotationObject;
+            _sensorData.ThisEntity = ThisEntity;
+            _sensorData.TargetFlag = _enemyFlag;
+
+            _sensorData.SensorRadius = _dynamicData.SensorRadius.Value;
+            _projectileData.Damage = _dynamicData.Damage.Value;
+
+            _dynamicData.SensorRadius.OnValueChanged += value => UpdateData();
+            _dynamicData.Damage.OnValueChanged += value => UpdateData();
+        }
+
+        private void UpdateData()
+        {
+            _sensorData.SensorRadius = _dynamicData.SensorRadius.Value;
+            _projectileData.Damage = _dynamicData.Damage.Value;
+        }
+
+        public void Dispose()
+        {
+            _dynamicData.SensorRadius.OnValueChanged -= value => UpdateData();
+            _dynamicData.Damage.OnValueChanged -= value => UpdateData();
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Content/BuildSystem/TurretLogic/TurretDatas/TurretDynamicData.cs
@@ -0,0 +1,40 @@
+﻿using Project.Content.ReactiveProperty;
+using UnityEngine;
+
+namespace Project.Content.BuildSystem
+{
+    public class TurretDynamicData
+    {
+        public readonly TurretType Type;
+
+        private readonly TurretConfig _config;
+
+        private float _maxHealth;
+        private float _fireRate;
+
+        private ReactiveProperty<float> _damage = new ReactiveProperty<float>();
+        private ReactiveProperty<float> _sensorRadius = new ReactiveProperty<float>();
+
+        public float MaxHealth => _maxHealth;
+        public float FireRate => _fireRate;
+        public IReactiveProperty<float> SensorRadius => _sensorRadius;
+        public IReactiveProperty<float> Damage => _damage;
+        public TurretConfig Config => _config;
+
+        public TurretDynamicData(TurretType type, TurretConfig config)
+        {
+            Type = type;
+            _config = config;
+
+            _maxHealth = _config.MaxHealth;
+            _fireRate = _config.FireRate;
+            _sensorRadius.Value = _config.SensorRadius;
+            _damage.Value = _config.ProjectileDamage;
+        }
+
+        public void IncreaseHealth(float value) => _maxHealth += Mathf.Abs(value);
+        public void IncreaseFireRate(float value) => _fireRate += Mathf.Abs(value);
+        public void IncreaseDamage(float value) => _damage.Value += Mathf.Abs(value);
+        public void IncreaseSensorRadius(float value) => _sensorRadius.Value += Mathf.Abs(value);
+    }
+}
