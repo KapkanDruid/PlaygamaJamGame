20a9d76c84f7feffd62bd7ca69a9155b180cfcc7
KapkanDruid <robloxxmapcreator1970@gmail.com>
2025-03-24 09:13:01 -0700

    feat: добавлена обработка поочередного размещения объектов

--- Assets/_Project/Scripts/Content/BuildSystem/PlaceLogic/BuildingSpawner.cs
+++ Assets/_Project/Scripts/Content/BuildSystem/PlaceLogic/BuildingSpawner.cs
@@ -1,25 +1,64 @@
-﻿using Zenject;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
 
 namespace Project.Content.BuildSystem
 {
     public class BuildingSpawner
     {
         private GridPlaceSystem _gridPlaceSystem;
+        private Queue<IEntity> _gridPlaceComponents = new Queue<IEntity>();
+        private bool _isPlacing;
 
         public BuildingSpawner(GridPlaceSystem gridPlaceSystem)
         {
             _gridPlaceSystem = gridPlaceSystem;
+            _gridPlaceSystem.OnPlaceComponentPlaced += OnBuildingPlaced;
         }
 
         public void Spawn<T>(PlaceholderFactory<T> factory) where T : IEntity
         {
             var placeEntity = factory.Create();
+
+            placeEntity.ProvideComponent<MonoBehaviour>().gameObject.SetActive(false);
+
+            _gridPlaceComponents.Enqueue(placeEntity);
+
+            PlaceBuilding();
+        }
+
+        private void PlaceBuilding()
+        {
+            if (_isPlacing)
+                return;
+
+            _isPlacing = true;
+            var placeEntity = _gridPlaceComponents.Dequeue();
+
             var placeComponent = placeEntity.ProvideComponent<GridPlaceComponent>();
 
             if (placeComponent != null)
             {
+                placeEntity.ProvideComponent<MonoBehaviour>().gameObject.SetActive(true);
+
                 _gridPlaceSystem.StartPlacing(placeComponent);
             }
+            else
+            {
+                Debug.LogError("[BuildingSpawner] Failed to spawn building, entity dos not have GridPlaceComponent");
+            }
+        }
+
+        private void OnBuildingPlaced()
+        {
+            if (_gridPlaceComponents.Count > 0)
+            {
+                _isPlacing = false;
+                PlaceBuilding();
+            }
+            else
+                _isPlacing = false;
+
         }
     }
 }
