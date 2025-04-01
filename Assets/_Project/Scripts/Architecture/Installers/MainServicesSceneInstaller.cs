040c4e094e62122bee9ee244cc23334e3fb029a5
KapkanDruid <robloxxmapcreator1970@gmail.com>
2025-04-01 05:31:27 -0700

    refactor: GridPlaceSystem переведен с MonoBehaviour на ITickable

--- Assets/_Project/Scripts/Architecture/Installers/MainServicesSceneInstaller.cs
+++ Assets/_Project/Scripts/Architecture/Installers/MainServicesSceneInstaller.cs
@@ -12,7 +12,6 @@ namespace Project.Architecture
     public class MainServicesSceneInstaller : MonoInstaller
     {
         [SerializeField] private MainSceneBootstrap _sceneBootstrap;
-        [SerializeField] private GridPlaceSystem _gridPlaceSystem;
         [SerializeField] private GizmosDrawer _gizmosDrawer;
         [SerializeField] private Grid _grid;
         [SerializeField] private NavMeshSurface _navMeshSurface;
@@ -27,7 +26,7 @@ namespace Project.Architecture
         public override void InstallBindings()
         {
             Container.Bind<MainSceneBootstrap>().FromInstance(_sceneBootstrap).AsSingle().NonLazy();
-            Container.Bind<GridPlaceSystem>().FromInstance(_gridPlaceSystem).AsSingle().NonLazy();
+            Container.BindInterfacesAndSelfTo<GridPlaceSystem>().AsSingle().NonLazy();
             Container.Bind<Grid>().FromInstance(_grid).AsSingle().NonLazy();
             Container.Bind<InputSystemActions>().AsSingle().NonLazy();
             Container.BindInterfacesAndSelfTo<PauseHandler>().AsSingle().NonLazy();
